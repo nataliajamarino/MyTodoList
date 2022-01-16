e93e9589502b673cd09a88068114c211d58eb74e
nataliajamarino <nataliajamarino@yahoo.com.br>
2022-01-16 15:56:31 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## My Todo List
+
+Small project for a simple todo list, HTML + CSS + Javascript
+
+# Running the project
+
+Just open the file index.html in the browser.
---
+++ index.js
@@ -0,0 +1,12 @@
+
+    function addToDo() {
+        var ul = document.getElementById("myUl");
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(document.getElementById("myInput").value));
+        ul.appendChild(li);
+    
+    } 
+    
+    var writeCheck = function (addToDo) {
+        if (myInput = null ) return
+    }
